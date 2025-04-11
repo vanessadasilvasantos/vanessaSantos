91bd292fea4842bf51947a681797167e4e714dca
vanessadasilvasantos <vanessadasilvasantos65@email.com>
2025-04-11 15:56:15 -0300

    Correcao de pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vanessadasilvasantos.github.io/vanessaSantos',
+  base: "/vanessaSantos",
   devToolbar: {
     enabled: false
   },
